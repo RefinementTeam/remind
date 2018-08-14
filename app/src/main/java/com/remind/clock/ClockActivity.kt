d843c7d2fc8a3db4193be84e7fedbca5264e28c6
lijiawei <lijiawei_855@126.com>
2018-08-14 23:29:46 +0800

    闹钟

--- app/src/main/java/com/remind/clock/ClockActivity.kt
+++ app/src/main/java/com/remind/clock/ClockActivity.kt
@@ -1,16 +1,48 @@
 package com.remind.clock
 
 import android.os.Bundle
+import android.support.v7.widget.AppCompatEditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.remind.BaseActivity
 import com.remind.R
+import com.remind.view.WheelView
 
 /**
  * 闹钟界面
  */
 class ClockActivity : BaseActivity() {
 
+    private lateinit var clock_back: ImageView
+    private lateinit var clock_save: TextView
+    private lateinit var click_content_edit: AppCompatEditText
+    private lateinit var clock_time_hour: WheelView
+    private lateinit var clock_time_min: WheelView
+    private lateinit var clock_weekday: TextView
+    private lateinit var clock_weekend: TextView
+    private lateinit var clock_clock_audio: ImageView
+    private lateinit var clock_clock_shake: ImageView
+    private lateinit var clock_audio_select_ll: LinearLayout
+    private lateinit var clock_audio_name: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clock)
+        init()
+    }
+
+    private fun init() {
+        clock_back = findViewById(R.id.clock_back)
+        clock_save = findViewById(R.id.clock_save)
+        click_content_edit = findViewById(R.id.click_content_edit)
+        clock_time_hour = findViewById(R.id.clock_time_hour)
+        clock_time_min = findViewById(R.id.clock_time_min)
+        clock_weekday = findViewById(R.id.clock_weekday)
+        clock_weekend = findViewById(R.id.clock_weekend)
+        clock_clock_audio = findViewById(R.id.clock_clock_audio)
+        clock_clock_shake = findViewById(R.id.clock_clock_shake)
+        clock_audio_select_ll = findViewById(R.id.clock_audio_select_ll)
+        clock_audio_name = findViewById(R.id.clock_audio_name)
     }
 }
---
+++ app/src/main/java/com/remind/clock/ClockAudioActivity.kt
@@ -0,0 +1,16 @@
+package com.remind.clock
+
+import android.os.Bundle
+import com.remind.BaseActivity
+import com.remind.R
+
+/**
+ * 音乐选择
+ */
+class ClockAudioActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clock_audio)
+    }
+}
